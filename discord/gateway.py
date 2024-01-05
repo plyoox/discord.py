f1805be789be11b702e291c81f714093c5785014
JohannesIBK <mail@greuter.dev>
2024-01-05 12:00:58 +0100

    add optional compression

--- discord/gateway.py
+++ discord/gateway.py
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
-
 from __future__ import annotations
 
 import asyncio
@@ -290,6 +289,8 @@ class DiscordWebSocket:
         The authentication token for discord.
     """
 
+    SHOULD_COMPRESS = True
+
     if TYPE_CHECKING:
         token: Optional[str]
         _connection: ConnectionState
@@ -362,7 +363,7 @@ class DiscordWebSocket:
         sequence: Optional[int] = None,
         resume: bool = False,
         encoding: str = 'json',
-        compress: bool = True,
+        compress: bool = SHOULD_COMPRESS,
     ) -> Self:
         """Creates a main websocket for Discord from a :class:`Client`.
 
@@ -457,7 +458,7 @@ class DiscordWebSocket:
                     'browser': 'discord.py',
                     'device': 'discord.py',
                 },
-                'compress': True,
+                'compress': self.SHOULD_COMPRESS,
                 'large_threshold': 250,
             },
         }
