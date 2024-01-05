b5117925cfe0a1bca9563efedf6438fb2c7b1aaf
JohannesIBK <mail@greuter.dev>
2024-01-05 12:00:58 +0100

    add optional compression

--- discord/gateway.py
+++ discord/gateway.py
@@ -282,6 +282,8 @@ class DiscordWebSocket:
         The authentication token for discord.
     """
 
+    SHOULD_COMPRESS = True
+
     if TYPE_CHECKING:
         token: Optional[str]
         _connection: ConnectionState
@@ -354,7 +356,7 @@ class DiscordWebSocket:
         sequence: Optional[int] = None,
         resume: bool = False,
         encoding: str = 'json',
-        compress: bool = True,
+        compress: bool = SHOULD_COMPRESS,
     ) -> Self:
         """Creates a main websocket for Discord from a :class:`Client`.
 
@@ -449,7 +451,7 @@ class DiscordWebSocket:
                     'browser': 'discord.py',
                     'device': 'discord.py',
                 },
-                'compress': True,
+                'compress': self.SHOULD_COMPRESS,
                 'large_threshold': 250,
             },
         }
