8e0063a64cdefb0502886541872f21f91166fbc8
JohannesIBK <mail@greuter.dev>
2024-02-03 12:17:45 +0100

    use functools instead of utils.cached_property for development

--- discord/interactions.py
+++ discord/interactions.py
@@ -26,6 +26,7 @@ DEALINGS IN THE SOFTWARE.
 
 from __future__ import annotations
 
+import functools
 import logging
 from typing import Any, Dict, Optional, Generic, TYPE_CHECKING, Sequence, Tuple, Union, List, overload
 import asyncio
@@ -328,7 +329,7 @@ class Interaction(Generic[ClientT]):
         """:class:`Permissions`: The resolved permissions of the application or the bot, including overwrites."""
         return Permissions(self._app_permissions)
 
-    @utils.cached_slot_property('_cs_namespace')
+    @functools.cached_property
     def namespace(self) -> Namespace:
         """:class:`app_commands.Namespace`: The resolved namespace for this interaction.
 
@@ -352,7 +353,7 @@ class Interaction(Generic[ClientT]):
 
         return Namespace(self, data.get('resolved', {}), options)
 
-    @utils.cached_slot_property('_cs_command')
+    @functools.cached_property
     def command(self) -> Optional[Union[Command[Any, ..., Any], ContextMenu]]:
         """Optional[Union[:class:`app_commands.Command`, :class:`app_commands.ContextMenu`]]: The command being called from
         this interaction.
@@ -380,7 +381,7 @@ class Interaction(Generic[ClientT]):
         else:
             return tree._get_context_menu(data)
 
-    @utils.cached_slot_property('_cs_command_id')
+    @functools.cached_property
     def command_id(self) -> Optional[int]:
         """Optional[:class:`int`]: The ID of the command that triggered this interaction.
 
@@ -395,7 +396,7 @@ class Interaction(Generic[ClientT]):
         data: ApplicationCommandInteractionData = self.data  # type: ignore
         return int(data.get('id', 0))
 
-    @utils.cached_slot_property('_cs_response')
+    @functools.cached_property
     def response(self) -> InteractionResponse[ClientT]:
         """:class:`InteractionResponse`: Returns an object responsible for handling responding to the interaction.
 
@@ -404,7 +405,7 @@ class Interaction(Generic[ClientT]):
         """
         return InteractionResponse(self)
 
-    @utils.cached_slot_property('_cs_followup')
+    @functools.cached_property
     def followup(self) -> Webhook:
         """:class:`Webhook`: Returns the follow up webhook for follow up interactions."""
         payload: WebhookPayload = {
